f799f3f20f8d3a78a2457b5df706dc512d9a66a5
Antonio <96915878+Ferno22@users.noreply.github.com>
2023-12-01 18:50:01 +0100

    Updated the theme for the popular now screen

--- lib/screens/popular_screen.dart
+++ lib/screens/popular_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mini_challenge_3/api/tmdb_api.dart';
 
 class PopularScreen extends StatefulWidget {
-  const PopularScreen({super.key});
+  const PopularScreen({Key? key}) : super(key: key);
 
   @override
   _PopularScreenState createState() => _PopularScreenState();
@@ -23,104 +23,106 @@ class _PopularScreenState extends State<PopularScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Popular Now'),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const [
-            Tab(text: 'Movies'),
-            Tab(text: 'Series'),
-          ],
+        backgroundColor: Colors.transparent,
+        elevation: 0, // Remove the shadow
+        title: Text(
+          'Popular Now',
+          style: TextStyle(color: Colors.white), // Customize title color
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: Colors.blueGrey[900],
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: TabBar(
+              controller: _tabController,
+              indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(color: Colors.cyanAccent, width: 1),
+              ),
+              labelColor:
+                  Colors.cyanAccent, // Customize text color for selected tab
+              unselectedLabelColor:
+                  Colors.white, // Customize text color for unselected tab
+              tabs: const [
+                Tab(
+                  text: 'Movies',
+                ),
+                Tab(
+                  text: 'Series',
+                ),
+              ],
+            ),
+          ),
         ),
       ),
       body: TabBarView(
         controller: _tabController,
         children: [
-          FutureBuilder(
-            future: _api.getPopularMovies(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else {
-                var movies = snapshot.data?['results'];
-                return GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10, // Add horizontal spacing
-                    mainAxisSpacing: 10, // Add vertical spacing
-                  ),
-                  itemCount: movies.length,
-                  itemBuilder: (context, index) {
-                    var movie = movies[index];
-                    return Column(
-                      children: [
-                        Expanded(
-                          child: Image.network(
-                            'https://image.tmdb.org/t/p/w500${movie['poster_path'] ?? ''}',
-                            width: double.infinity,
-                          ),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          color: Colors.black54,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(movie['title'] ?? '',
-                                style: const TextStyle(color: Colors.white)),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              }
-            },
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: _buildMediaGrid(_api.getPopularMovies()),
           ),
-          FutureBuilder(
-            future: _api.getPopularSeries(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else {
-                var series = snapshot.data?['results'];
-                return GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10, // Add horizontal spacing
-                    mainAxisSpacing: 10, // Add vertical spacing
-                  ),
-                  itemCount: series.length,
-                  itemBuilder: (context, index) {
-                    var serie = series[index];
-                    return Column(
-                      children: [
-                        Expanded(
-                          child: Image.network(
-                            'https://image.tmdb.org/t/p/w500${serie['poster_path'] ?? ''}',
-                            width: double.infinity,
-                          ),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          color: Colors.black54,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(serie['name'] ?? '',
-                                style: const TextStyle(color: Colors.white)),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              }
-            },
-          )
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: _buildMediaGrid(_api.getPopularSeries()),
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _buildMediaGrid(Future<Map<String, dynamic>> future) {
+    return FutureBuilder(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          var mediaList = snapshot.data?['results'];
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemCount: mediaList.length,
+            itemBuilder: (context, index) {
+              var media = mediaList[index];
+              return Container(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Image.network(
+                        'https://image.tmdb.org/t/p/w500${media['poster_path'] ?? ''}',
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      color: Colors.blueGrey[900],
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          media['title'] ?? media['name'] ?? '',
+                          style: const TextStyle(color: Colors.white),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
